ea58fbe691559b2f13210716a66bdee70f74be49
御剑 <lingen.liu@gmail.com>
2022-06-15 10:00:47 +0800

    feat: 升级android gradle plugin至7.2.1版本

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ buildscript {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("org.jetbrains.kotlin:kotlin-serialization:$kotlinVersion")
         classpath("com.squareup.sqldelight:gradle-plugin:$sqlDelightVersion")
-        classpath("com.android.tools.build:gradle:7.2.0")
+        classpath("com.android.tools.build:gradle:7.2.1")
     }
 }
 
